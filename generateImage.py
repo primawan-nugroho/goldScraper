979ef97eaa04e7a4e2a26ba937c05a711b9c5465
primawan-nugroho <74648388+primawan-nugroho@users.noreply.github.com>
2021-08-15 20:50:41 +0700

    update get image file from url

--- generateImage.py
+++ generateImage.py
@@ -1,21 +1,25 @@
 # importing image object from PIL
-import math
 from PIL import Image, ImageDraw, ImageFont
 import datetime
+import requests
+
 
 def drawPrice(antamIndogoldPrice, UBSIndogoldPrice, antamLMPrice):
+    url_blankdesign = 'https://github.com/primawan-nugroho/goldScraper/blob/master/Resources/blank_design.jpg?raw=true'
+    url_poppins_semibold = 'https://github.com/primawan-nugroho/goldScraper/blob/master/Resources/Poppins/Poppins-SemiBold.ttf?raw=true'
+    url_poppins_medium = 'https://github.com/primawan-nugroho/goldScraper/blob/master/Resources/Poppins/Poppins-Medium.ttf?raw=true'
     W = 1600
     today = datetime.date.today().strftime('%d-%B-%Y')
-    img = Image.open(r'D:\Python\goldScraper\Resources\blank_design.jpg')
+    img = Image.open(requests.get(url_blankdesign, stream=True).raw)
     canvas = ImageDraw.Draw(img)
 
     # draw today in center alignment
-    font = ImageFont.truetype(r'D:\Python\goldScraper\Resources\Poppins\Poppins-SemiBold.ttf', 70)
+    font = ImageFont.truetype(requests.get(url_poppins_semibold, stream=True).raw, 70)
     text_width, text_height = canvas.textsize(today, font)
     position = ((W-text_width)/2, 193)
     canvas.text(position, today, (255,255,255),font=font)
 
-    font = ImageFont.truetype(r'D:\Python\goldScraper\Resources\Poppins\Poppins-SemiBold.ttf', 100)
+    font = ImageFont.truetype(requests.get(url_poppins_semibold, stream=True).raw, 100)
 
     # draw harga antam indogold
     canvas.text((780, 490), antamIndogoldPrice,(255,255,255),font=font)
@@ -27,7 +31,7 @@ def drawPrice(antamIndogoldPrice, UBSIndogoldPrice, antamLMPrice):
     canvas.text((780, 1250),antamLMPrice,(255,255,255),font=font)
 
     # draw gram
-    font = ImageFont.truetype(r'D:\Python\goldScraper\Resources\Poppins\Poppins-Medium.ttf', 50)
+    font = ImageFont.truetype(requests.get(url_poppins_medium, stream=True).raw, 50)
     canvas.text((1200, 550),"/gr",(255,255,255),font=font)
     canvas.text((1200, 840),"/gr",(255,255,255),font=font)
     canvas.text((1200, 1310),"/gr",(255,255,255),font=font)
