24aee53c3b04ddca991a5ccdc9438f75dadc0030
primawan-nugroho <74648388+primawan-nugroho@users.noreply.github.com>
2021-08-16 15:17:46 +0700

    debug none attr soup

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+'''
+TODO:
+Test schedule run in Heroku
+[worker.1]: Traceback (most recent call last):
+[worker.1]:   File "/app/main.py", line 37, in <module>
+[worker.1]:     main()
+[worker.1]:   File "/app/main.py", line 33, in main
+[worker.1]:     schedule.run_pending()
+[worker.1]:   File "/app/.heroku/python/lib/python3.9/site-packages/schedule/__init__.py", line 780, in run_pending
+[worker.1]:     default_scheduler.run_pending()
+[worker.1]:   File "/app/.heroku/python/lib/python3.9/site-packages/schedule/__init__.py", line 100, in run_pending
+[worker.1]:     self._run_job(job)
+[worker.1]:   File "/app/.heroku/python/lib/python3.9/site-packages/schedule/__init__.py", line 172, in _run_job
+[worker.1]:     ret = job.run()
+[worker.1]:   File "/app/.heroku/python/lib/python3.9/site-packages/schedule/__init__.py", line 661, in run
+[worker.1]:     ret = self.job_func()
+[worker.1]:   File "/app/main.py", line 14, in perform
+[worker.1]:     antamLM = goldScraper.getAntamLM()
+[worker.1]:   File "/app/goldScraper.py", line 13, in getAntamLM
+[worker.1]:     ans = soup.find("td", text="1 gr").find_next_sibling("td").text
+[worker.1]: AttributeError: 'NoneType' object has no attribute 'find_next_sibling'
+2021-08-16T06:00:05.706374+00:00 heroku[worker.1]: Process exited with status 1
+'''
 from instabot import bot
 import goldScraper
 from datetime import date, datetime
@@ -24,12 +47,12 @@ def perform():
     config.clean_up()    
     config.postIG(bot,"Post {}.jpg".format(today))
 
+    print("next schedule : {} {}".format(today,TIME))
+
 def main():
     schedule.every().day.at(TIME).do(perform)
     while True:
-        today = date.today().strftime('%d-%B-%Y')    
-        #current_time = datetime.now().strftime("%H:%M:%S") 
-        print("scheduled : {} {}".format(today, TIME))
+        #current_time = datetime.now().strftime("%H:%M:%S")
         schedule.run_pending()
         time.sleep(10)
 
