c2dbb0638195659d28b3d843f7e0a34caa65baf1
primawan-nugroho <74648388+primawan-nugroho@users.noreply.github.com>
2021-08-16 10:09:03 +0700

    Fix error 429

--- config.py
+++ config.py
@@ -2,32 +2,35 @@ from instabot import Bot
 from datetime import date
 import os
 import shutil
+import time
 
 def postIG(file):
     today = date.today().strftime('%d-%B-%Y')
     bot = Bot()
+    time.sleep(5)
     bot.login(username = os.getenv("WHOAREYOU"),
               password = os.getenv("IAMYOU"))
+    time.sleep(5)
     bot.upload_photo(file,
                caption = "Harga emas hari ini {}".format(today))
+    time.sleep(5)
     bot.logout()
 
 def clean_up():
+    # check folder config dan file REMOVE_ME exist atau tidak
+    # kalo exist, hapus!!
+    # bug di instabot gabisa upload kalo ada 2 file/folder diatas
     today = date.today().strftime('%d-%B-%Y')
     dir = "config"
     img = "Post {}.jpg.REMOVE_ME".format(today)
 
-    # checking whether config folder exists or not
     if os.path.exists(dir):
         try:
-            # removing it because in 2021 it makes problems with new uploads
             shutil.rmtree(dir)
         except OSError as e:
             print("Error: %s - %s." % (e.filename, e.strerror))
-    # checking whether REMOVE_ME file exists or not
     if os.path.exists(img):
         try:
-            # removing it because in 2021 it makes problems with new uploads
             os.remove(img)
         except OSError as e:
             print("Error: %s - %s." % (e.filename, e.strerror))
